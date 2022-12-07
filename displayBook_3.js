dd3e4825274c2922ca7e7b16e40c9284d5ebc9f3
unknown <kavyasandi.99@gmail.com>
2022-12-06 20:41:15 -0500

    do

---
+++ displayBook_3.js
@@ -0,0 +1,40 @@
+//https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb-how-to-get-connected-to-your-database 
+const {MongoClient} = require('mongodb');
+
+ 
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri ="mongodb+srv://hello1:123@cluster0.fsqirco.mongodb.net/?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);   
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        //await  listDatabases(client);
+        await findsomedata(client);
+ 
+    } catch (e) {
+        console.error(e);                    
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+
+async function findsomedata(client ){
+    const cursor = client.db("bookdb").collection("bookcollection").find({});
+    const results = await cursor.toArray();
+    //console.log(results);
+    const js= (JSON.stringify(results));
+    console.log(js);
+
+};
